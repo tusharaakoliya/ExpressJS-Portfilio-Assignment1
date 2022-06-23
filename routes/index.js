c972038229a5695d4eaa00543a52d4ebbe9fe646
tushar <tusharaakoliya001@gmail.com>
2022-06-23 10:25:11 -0400

    half way done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Portfolio' });
 });
 
+
+// GET Projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' })
+});
+
+// GET about me page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' })
+});
+
+// GET about me page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' })
+});
 module.exports = router;
